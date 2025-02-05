3484551de2ba12a2bcbec0a9c34a1ec5ef651d6f
oramirezperera <ramirez.orlando17@gmail.com>
2025-02-05 17:44:22 -0400

    changes to charts.py now we save the bars as the country name

--- app/charts.py
+++ app/charts.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(labels, values):
+def generate_bar_chart(name, labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.savefig('bar.png')
+    plt.savefig(f'bar.png')
     plt.close()
 
-def generate_pie_chart(labels, values):
+def generate_pie_chart(name, labels, values):
 
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.savefig('pie.png')
+    plt.savefig(f'imgs/{name}pie.png')
     plt.close()
 
 if __name__ == '__main__':
