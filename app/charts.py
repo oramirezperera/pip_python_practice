191e8fda6459c46484e09f85a19611ba50894181
oramirezperera <ramirez.orlando17@gmail.com>
2025-02-05 17:51:47 -0400

    changes to charts and main now works and creates an image with the name of the country in the barchart

--- app/charts.py
+++ app/charts.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(name, labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.savefig(f'bar.png')
+    plt.savefig(f'imgs/{name}bar.png')
     plt.close()
 
-def generate_pie_chart(name, labels, values):
+def generate_pie_chart(labels, values):
 
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.savefig(f'imgs/{name}pie.png')
+    plt.savefig(f'pie.png')
     plt.close()
 
 if __name__ == '__main__':
