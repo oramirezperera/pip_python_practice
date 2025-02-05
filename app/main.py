ae1ce3601a1523671626e0da5a10cfa53eabdfd0
oramirezperera <ramirez.orlando17@gmail.com>
2025-02-05 17:15:19 -0400

    changes to main.py in app

--- app/main.py
+++ app/main.py
@@ -12,5 +12,15 @@ def run():
 
     charts.generate_pie_chart(countries, percentages)
 
+    country = input('Type Country => ')
+
+    result = utils.population_by_country(data, country)
+
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
+
+
 if __name__ == '__main__':
     run()
