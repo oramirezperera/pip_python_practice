5bdfffbc6502d710b2c805b172eab622d7702cf1
oramirezperera <ramirez.orlando17@gmail.com>
2025-02-05 16:56:09 -0400

    added data.csv and main.py

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import utils
+import read_csv
+import charts
+
+def run():
+    data = read_csv.read_csv('./app/data.csv')
+    data = list(filter(lambda item : item['continent'] == 'South America', data))
+
+    countries = list(map(lambda x : x['Country/Territory'], data))
+
+    percentages = list(map(lambda x : x['World Population Percentage'], data))
+
+    charts.generate_pie_chart(countries, percentages)
