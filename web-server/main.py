872431990cb9e97d8ac0c378def543aa2160d854
oramirezperera <ramirez.orlando17@gmail.com>
2025-02-07 10:15:42 -0400

    changes to main.py now works with fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def contact():
+    return """
+        <h1> Soy un titulo </h1>
+        <p> Soy un parrafo </p>
+    """
+
+
 
 def run():
     store.get_categories()
